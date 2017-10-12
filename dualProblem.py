97c3943ad5e44708fed76cb3f1b8ad51b7889ea2
ErikOrm <erik.o.hellsten@gmail.com>
2017-10-12 09:22:52 +0200

    Update dualProblem.py

--- dualProblem.py
+++ dualProblem.py
@@ -11,8 +11,6 @@ def dual_problem(n_c, n_r, n_n, n_ec, A, c, d):
     nodes = ["n%i" % (i+1+n_n) for i in range(n_n)] + ["n%i" % (i+1+2*n_n+n_c) for i in range(n_ec)]
     cars = ["c%i" % (i+1) for i in range(n_c)]
     
-    # create a dictionary of pulp variables with keys from ingredients
-    ## the default lower bound is -inf
     pi = pulp.LpVariable.dict('pi%s', nodes, lowBound = 0)
     gamma = pulp.LpVariable.dict('gamma%s', cars, upBound = 0)
     
